eb33c5c8888756e42984689a5f9e236cc0271471
Benny-Code95 <1287343797@qq.com>
2020-11-12 21:31:00 +0800

    scrapy

---
+++ MySpider/spiders/news.py
@@ -0,0 +1,27 @@
+import scrapy
+import os
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    # allowed_domains = ['haiwainet.cn']
+    start_urls = ['http://finance.haiwainet.cn/yw/']
+    custom_settings = {'LOG_LEVEL': 'WARNING',
+                       'LOG_FILE': '../logs/'+name+'.log'}
+
+
+    # def start_requests(self):
+    #     yield scrapy.Request(self.start_urls[0])
+
+    def parse(self, response):
+        ls = response.xpath('//ul[@class="ul04 ub01 pt10 upton"]')
+        item = {}
+        for l in ls:
+            url = l.xpath('./li/a/@href').extract_first()
+            item['detail'] = url
+            break
+        yield item
+
+
+if __name__ == '__main__':
+    os.system('scrapy crawl news')
