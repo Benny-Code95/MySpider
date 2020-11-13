56c02e043c0ae6a1268c08e1ccf8298ea7c583bc
Benny-Code95 <1287343797@qq.com>
2020-11-13 10:36:42 +0800

    scrapy

--- MySpider/spiders/news.py
+++ MySpider/spiders/news.py
@@ -5,23 +5,15 @@ import os
 class NewsSpider(scrapy.Spider):
     name = 'news'
     # allowed_domains = ['haiwainet.cn']
-    start_urls = ['http://finance.haiwainet.cn/yw/']
+    start_urls = ['https://www.cnki.net/']
     custom_settings = {'LOG_LEVEL': 'WARNING',
                        'LOG_FILE': '../logs/'+name+'.log'}
-
 
     # def start_requests(self):
     #     yield scrapy.Request(self.start_urls[0])
 
     def parse(self, response):
-        ls = response.xpath('//ul[@class="ul04 ub01 pt10 upton"]')
-        item = {}
-        for l in ls:
-            url = l.xpath('./li/a/@href').extract_first()
-            item['detail'] = url
-            break
-        pass
-        yield item
+        print(response.text)
 
 
 if __name__ == '__main__':
